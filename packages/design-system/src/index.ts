7c26000c386d4a61c6f94be57d0f0a0b15dd8e00
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-07-13 07:46:16 +0530

    Add resxports to src, rearrange alphabetically

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,11 +1,19 @@
-export { default as TooltipComponent } from "./Tooltip";
-export * from "./Tooltip";
-
-export { default as Text } from "./Text";
-export * from "./Text";
-
 export { default as AppIcon } from "./AppIcon";
 export * from "./AppIcon";
 
 export { default as DatePicker } from "./DatePicker";
 export * from "./DatePicker";
+
+export * from "./DropdownV2";
+
+export { HighlightText } from "./HighlightText";
+export * from "./HighlightText";
+
+export { default as SearchComponent } from "./SearchComponent";
+export * from "./SearchComponent";
+
+export { default as Text } from "./Text";
+export * from "./Text";
+
+export { default as TooltipComponent } from "./Tooltip";
+export * from "./Tooltip";
