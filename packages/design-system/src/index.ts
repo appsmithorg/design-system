455623a50dcb8240f6de706bc70ee63fc5298b36
Albin <albin@appsmith.com>
2022-07-22 12:09:00 +0530

    Added FieldError component

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -47,6 +47,9 @@ export * from "./EmojiPicker";
 export { default as EmojiReactions } from "./EmojiReactions";
 export * from "./EmojiReactions";
 
+export { default as FieldError } from "./FieldError";
+export * from "./FieldError";
+
 export { default as GifPlayer } from "./GifPlayer";
 export * from "./GifPlayer";
 
