3e1ae92d11ed6ed854b6b9128a3a87054d882059
Albin <albin@appsmith.com>
2022-09-05 14:13:39 +0530

    feat: support for graphql pagination - added selectedOptionElements

--- packages/design-system/src/Radio/index.tsx
+++ packages/design-system/src/Radio/index.tsx
@@ -17,6 +17,7 @@ export type RadioProps = CommonComponentProps & {
   defaultValue: string;
   onSelect?: (value: string) => void;
   options: OptionProps[];
+  selectedOptionElements?: Array<JSX.Element | null>;
   backgroundColor?: string;
   // To prevent interference when there are multiple radio groups,
   // options corresponding to the same radio should have same name, which is different from others.
@@ -128,9 +129,12 @@ export default function RadioComponent(props: RadioProps) {
     setSelected(props.defaultValue);
   }, [props.defaultValue]);
 
-  const onChangeHandler = (value: string) => {
-    setSelected(value);
-    props.onSelect && props.onSelect(value);
+  const onChangeHandler = (e: any) => {
+    if (!e.target?.closest(".t--radioSelectedOptionElement")) {
+      const value: string = e.target.value;
+      setSelected(value);
+      props.onSelect && props.onSelect(value);
+    }
   };
 
   return (
@@ -138,28 +142,37 @@ export default function RadioComponent(props: RadioProps) {
       className={props.className}
       columns={props.columns}
       data-cy={props.cypressSelector}
-      onChange={(e: any) => onChangeHandler(e.target.value)}
+      onChange={onChangeHandler}
       rows={props.rows}
     >
       {props.options.map((option: OptionProps, index: number) => (
-        <Radio
-          backgroundColor={props.backgroundColor}
-          columns={props.columns}
-          disabled={props.disabled || option.disabled}
-          key={index}
-          rows={props.rows}
-        >
-          {option.label}
-          <input
-            checked={selected === option.value}
+        <React.Fragment key={index}>
+          <Radio
+            backgroundColor={props.backgroundColor}
+            columns={props.columns}
             disabled={props.disabled || option.disabled}
-            name={props.name || "radio"}
-            onChange={(e) => option.onSelect && option.onSelect(e.target.value)}
-            type="radio"
-            value={option.value}
-          />
-          <span className="checkbox" />
-        </Radio>
+            key={index}
+            rows={props.rows}
+          >
+            {option.label}
+            <input
+              checked={selected === option.value}
+              disabled={props.disabled || option.disabled}
+              name={props.name || "radio"}
+              onChange={(e) =>
+                option.onSelect && option.onSelect(e.target.value)
+              }
+              type="radio"
+              value={option.value}
+            />
+            <span className="checkbox" />
+          </Radio>
+          {selected === option.value && (
+            <div className="t--radioSelectedOptionElement">
+              {props.selectedOptionElements?.[index]}
+            </div>
+          )}
+        </React.Fragment>
       ))}
     </RadioGroup>
   );
