65c14887a693f894588668753167d75b9614c341
Albin <albin@appsmith.com>
2022-09-30 00:39:50 +0530

    feat: form group component migrated

---
+++ packages/design-system/src/FormGroup/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { FormGroup, Classes } from "@blueprintjs/core";
+import { getTypographyByKey } from "Common/index";
+
+type FormGroupProps = {
+  fill?: boolean;
+};
+const StyledFormGroup = styled(FormGroup)<FormGroupProps>`
+  & {
+    width: ${(props) => (props.fill ? "100%" : "auto")};
+    &.${Classes.FORM_GROUP} {
+      margin: 0 0 var(--ads-spaces-11);
+    }
+    &.${Classes.FORM_GROUP} .${Classes.FORM_HELPER_TEXT} {
+      font-size: var(--ads-font-size-3);
+    }
+    &.${Classes.FORM_GROUP} .${Classes.LABEL} {
+      ${getTypographyByKey("h5")}
+      color: var(--ads-form-group-label-text-color);
+    }
+  }
+`;
+export default StyledFormGroup;
