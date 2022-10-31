0cfd71f7753e4a281c10afacf99dec694843c91b
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-10-31 07:13:11 +0530

    Add props to control justification of content in box (and default props)

--- packages/design-system/src/Box/index.tsx
+++ packages/design-system/src/Box/index.tsx
@@ -4,6 +4,13 @@ import styled from "styled-components";
 type BoxProps = {
   space?: string;
   direction?: "horizontal" | "vertical";
+  justifyContent?:
+    | "space-evenly"
+    | "space-around"
+    | "space-between"
+    | "center"
+    | "flex-start"
+    | "flex-end";
   children: ReactNode;
 };
 
@@ -12,15 +19,24 @@ const StyledBox = styled.div<BoxProps>`
   border: 1px solid #f1f5f9;
   padding: ${(props) => props.space} 0px;
   display: flex;
-  justify-content: center;
+  justify-content: ${(props) => props.justifyContent};
 `;
 
-const Box = (props: BoxProps) => {
+const Box = ({
+  direction = "horizontal",
+  justifyContent = "space-evenly",
+  ...props
+}: BoxProps) => {
   return (
-    <StyledBox direction={props.direction} space={props.space}>
+    <StyledBox
+      direction={direction}
+      justifyContent={justifyContent}
+      space={props.space}
+    >
       {props.children}
     </StyledBox>
   );
 };
+1;
 
 export default Box;
