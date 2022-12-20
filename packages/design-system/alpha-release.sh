99a0fb0d8eb694b738af2bdb4c54956b21f52c05
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-12-20 19:23:53 +0530

    feat: tag the latest commit with the alpha tag created

--- packages/design-system/alpha-release.sh
+++ packages/design-system/alpha-release.sh
@@ -10,3 +10,7 @@ yarn build
 
 echo "==============Yarn build=============="
 npm publish --access public --tag alpha
+
+echo "==============Tag alpha on git =============="
+tag=$(npm dist-tag | grep "alpha" | awk '{print $2}')
+git tag -a "$tag" -m "tag ${tag}"
