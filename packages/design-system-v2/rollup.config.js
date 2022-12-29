8af38c6d89fed53dab5c6e97aa99ef5f90bb8283
Albin <albin@appsmith.com>
2022-12-29 20:41:51 +0530

    Basic setup

---
+++ packages/design-system-v2/rollup.config.js
@@ -0,0 +1,40 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import url from "@rollup/plugin-url";
+import svgr from "@svgr/rollup";
+import postcss from "rollup-plugin-postcss";
+import path from "path";
+import postcssImport from "postcss-import";
+import { terser } from "rollup-plugin-terser";
+import image from "@rollup/plugin-image";
+
+export default {
+  // TODO: Figure out regex where each directory can be a separate module without having to manually add them
+  input: ["src/index.ts"],
+  output: [
+    {
+      dir: "build",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  preserveModules: true,
+  plugins: [
+    peerDepsExternal(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      minimize: true,
+      sourceMap: true,
+      plugins: [postcssImport()],
+      extract: path.resolve("build/css/design-system-v2.css"),
+    }),
+    url(),
+    svgr({
+      dimensions: false,
+    }),
+    terser(),
+    image(),
+  ],
+};
