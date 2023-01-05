1d8c986c476ea59ec18b68b6462768e43752d13a
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-01-05 18:41:01 +0530

    remove unused useToggle hook

--- packages/design-system-v2/src/Toggle/Toggle.tsx
+++ packages/design-system-v2/src/Toggle/Toggle.tsx
@@ -2,14 +2,6 @@ import React from "react";
 import { StyledToggle } from "./Toggle.styles";
 import { ToggleProps } from "./Toggle.types";
 
-export default function useToggle(initialValue = false) {
-  const [value, setValue] = React.useState(initialValue);
-  const toggle = React.useCallback(() => {
-    setValue((v) => !v);
-  }, []);
-  return [value, toggle];
-}
-
 const Toggle = (props: ToggleProps) => {
   const { checked, ...rest } = props;
 
