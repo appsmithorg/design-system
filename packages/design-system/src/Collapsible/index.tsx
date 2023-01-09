7277b18f5cb3e1e6019128f57ad767db5524a939
Sangeeth Sivan <sangeeth@appsmith.com>
2023-01-09 19:56:52 +0530

    feat: collapsible component

---
+++ packages/design-system/src/Collapsible/index.tsx
@@ -0,0 +1,63 @@
+import Icon, { IconSize } from "Icon";
+import React, { useState } from "react";
+import styled from "styled-components";
+import Text, { TextType } from "Text";
+
+const MenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: max-content;
+  gap: 14px;
+`;
+
+const MenuHeader = styled.div`
+  cursor: pointer;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+const MenuBody = styled.div<{ isOpen: boolean }>`
+  background-color: #fff;
+  color: #333;
+  display: ${(props) => (props.isOpen ? "flex" : "none")};
+  overflow: hidden;
+  margin-left: 30px;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+export interface CollapsibleMenuProps {
+  title: string;
+  children: React.ReactNode;
+  leftIcon?: string;
+  showIcon?: boolean;
+  rightIcon?: string;
+}
+
+export function Collapsible(props: CollapsibleMenuProps) {
+  const { children, leftIcon, rightIcon, showIcon = true, title } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <MenuContainer>
+      <MenuHeader onClick={() => setIsOpen(!isOpen)}>
+        {showIcon && (
+          <Icon
+            name={leftIcon ? leftIcon : isOpen ? "expand-more" : "expand-less"}
+            size={IconSize.XXL}
+          />
+        )}
+        <Text type={TextType.H3}>{title}</Text>
+        {rightIcon && <Icon name={rightIcon} size={IconSize.XXL} />}
+      </MenuHeader>
+      <MenuBody isOpen={isOpen}>{children}</MenuBody>
+    </MenuContainer>
+  );
+}
+
+Collapsible.Header = MenuHeader;
+Collapsible.Body = MenuBody;
+
+export default Collapsible;
