86569d4deeee8ecaa0fcd30d7be1a408195c9448
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-01-11 08:20:08 +0530

    Add templates and insert them into the files on generation.

--- packages/design-system-v2/create_new_component.sh
+++ packages/design-system-v2/create_new_component.sh
@@ -1,16 +1,24 @@
 # Accept the name of the component being created
-component=$1
+component=${1# }
 
 # Define the file structure we desire
 file_structure="
 src/$component/$component.stories.tsx
 src/$component/$component.styles.tsx
-src/$component/$component.tsx
 src/$component/$component.test.tsx
-src/$component/$component.types.tsx
+src/$component/$component.tsx
+src/$component/$component.types.ts
 src/$component/index.ts"
 
 # Create the file structure we desire
 mkdir "src/$component"
 echo "$file_structure" | sed '/^$/d;s/ /\//g' | xargs touch
 
+# add the templates to the created files
+cat templates/stories.txt >> src/"$component"/"$component".stories.tsx
+cat templates/styles.txt >> src/"$component"/"$component".styles.tsx
+cat templates/component.txt >> src/"$component"/"$component".tsx
+cat templates/types.txt >> src/"$component"/"$component".types.ts
+cat templates/index.txt >> src/"$component"/index.ts
+
+# TODO: Replace the component name in the files
