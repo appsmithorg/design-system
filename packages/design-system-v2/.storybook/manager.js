57bca18779115d901ba10a4861cdcc61896dce54
Arsalan <arsalanyaldram0211@outlook.com>
2023-01-13 17:01:44 +0530

    feat: added vite, removed storybooks folder.

--- packages/design-system-v2/.storybook/manager.js
+++
@@ -1,7 +0,0 @@
-import { addons } from "@storybook/addons";
-import appsmithTheme from "./AppsmithTheme";
-
-addons.setConfig({
-  theme: appsmithTheme,
-  selectedPanel: "ds-test"
-});
---
+++ packages/design-system-v2/vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import typescript from "rollup-plugin-typescript2";
+import { resolve } from "path";
+
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
+  build: {
+    outDir: "build",
+    sourcemap: true,
+    lib: {
+      name: "design-system-v2",
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      plugins: [
+        typescript({
+          tsconfig: "./tsconfig.json",
+        }),
+      ],
+      output: {
+        assetFileNames: ({ name = "" }) => {
+          /**
+           * Vite produces a single file style.css
+           * move it to the css folder in the build
+           */
+          if (name === "style.css") {
+            return "css/design-system-v2.css";
+          }
+          return name;
+        },
+        globals: {
+          react: "React",
+          "react-dom": "ReactDom",
+        },
+      },
+    },
+  },
+});
