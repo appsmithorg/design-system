0b30e09a3a02757c0ceed84e0e311c63ff624c31
Albin <albin@appsmith.com>
2023-01-16 11:46:42 +0530

    removed un wanted files

--- packages/design-system-v2/src/Input/Input.types.tsx
+++
@@ -1,8 +0,0 @@
-import 
-
-// Input props
-export type InputProps = {
-  /** (try not to) pass addition classes here */
-  className?: string;
-  /** label attribute */
-} & React.InputHTMLAttributes<HTMLInputElement>;
