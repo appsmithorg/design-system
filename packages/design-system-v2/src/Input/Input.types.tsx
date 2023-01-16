1d71a3df97221c17b4356ab76384e3f807d920d2
Albin <albin@appsmith.com>
2023-01-16 11:07:52 +0530

    added initial config for input

---
+++ packages/design-system-v2/src/Input/Input.types.tsx
@@ -0,0 +1,8 @@
+import 
+
+// Input props
+export type InputProps = {
+  /** (try not to) pass addition classes here */
+  className?: string;
+  /** label attribute */
+} & React.InputHTMLAttributes<HTMLInputElement>;
