908c6cf70cf9dc1eb5cf5fc57f41cfaac26bae6b
Albin <albin@appsmith.com>
2023-02-06 11:28:39 +0530

    classes -> constants rename in plop config

--- packages/design-system/plopfile.mjs
+++ packages/design-system/plopfile.mjs
@@ -35,8 +35,13 @@ export default function (plop) {
       },
       {
         type: "add",
-        path: "src/{{capitalize name}}/{{capitalize name}}.classes.ts",
-        templateFile: "plop-templates/classes.ts.hbs",
+        path: "src/{{capitalize name}}/{{capitalize name}}.types.ts",
+        templateFile: "plop-templates/types.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "src/{{capitalize name}}/{{capitalize name}}.constants.ts",
+        templateFile: "plop-templates/constants.ts.hbs",
       },
     ],
   });
