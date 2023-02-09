0716fb53bebcc5079b236d6c25aeb10c5ba661e4
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-02-09 16:37:50 +0530

    Add additional field to story

--- packages/design-system/src/Toast/Toast.stories.tsx
+++ packages/design-system/src/Toast/Toast.stories.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import { Toast, toast } from "./Toast";
-import { ToastProps } from "./Toast.types";
 
 export default {
   title: "Design System/Toast",
   component: Toast,
+  argTypes: {
+    content: {
+      control: "text",
+    },
+  },
 } as ComponentMeta<typeof Toast>;
 
 // eslint-disable-next-line react/function-component-definition
 const Template: ComponentStory<typeof Toast> = (args) => {
-  const notify = () =>
-    toast("Filepicker was removed", { ...args });
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const notify = () => toast(args.content, { ...args });
 
   return (
     <div>
@@ -23,11 +28,16 @@ const Template: ComponentStory<typeof Toast> = (args) => {
 };
 
 export const Default = Template.bind({});
+Default.args = {
+  content: "Am I a toast?",
+};
 
 export const ToastWithAction = Template.bind({});
 ToastWithAction.args = {
+  content: "Widget was removed",
+  kind: "success",
   action: {
     actionText: "undo",
     effect: () => console.log("effect affected"),
-  }
-}
+  },
+};
