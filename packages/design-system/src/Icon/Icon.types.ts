55c73e64fe355c39dae9e31962fc16eec17f709a
Albin <albin@appsmith.com>
2023-02-09 15:41:14 +0530

    Removed comment regarding xxxl

--- packages/design-system/src/Icon/Icon.types.ts
+++ packages/design-system/src/Icon/Icon.types.ts
@@ -5,7 +5,7 @@ export type IconSizes = Extract<Sizes, "sm" | "md" | "lg" | "xl" | "xxl">;
 export type IconProps = {
   /** name of the icon from remix icon library */
   name?: string;
-  /** size of the icon - sm,md,lg,xl, xxl, xxxl */
+  /** size of the icon - sm,md,lg,xl, xxl */
   size: IconSizes;
   /** color of the icon */
   color?: string;
