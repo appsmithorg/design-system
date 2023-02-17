2d3235c5d46841acaf5f088bd94f163dc2794fbd
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-02-17 13:35:02 +0530

    better types for link props

--- packages/design-system/src/Callout/Callout.types.ts
+++ packages/design-system/src/Callout/Callout.types.ts
@@ -1,7 +1,10 @@
 import React from "react";
 import { Kinds } from "../__config__/types";
+import { LinkProps } from "../Link/Link.types";
 
 export type Kind = Kinds;
+
+type CalloutLinkProps = Omit<LinkProps, "className, kind">;
 
 export type CalloutProps = {
   /** (try not to) pass addition classes here */
@@ -13,12 +16,5 @@ export type CalloutProps = {
   /** whether or not the callout should be closable */
   isClosable?: boolean;
   /** any additional links that might be present in the callout */
-  links?: [
-    {
-      text: string;
-      to: string;
-      startIcon?: string;
-      endIcon?: string;
-    },
-  ];
+  links?: CalloutLinkProps[];
 } & React.HTMLAttributes<HTMLDivElement>;
