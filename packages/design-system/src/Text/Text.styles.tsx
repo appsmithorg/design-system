5bdde3ad86007796f785cacd23dfb282b2bf2096
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-02-23 10:17:32 +0530

    Add line height, font weight, and letter spacing properties

--- packages/design-system/src/Text/Text.styles.tsx
+++ packages/design-system/src/Text/Text.styles.tsx
@@ -1,43 +1,6 @@
 import styled, { css } from "styled-components";
 import { Kind } from "./Text.types";
 
-const TypographyScales = css`
-  --ads-v2-h1-font-size: var(--ads-v2-font-size-10);
-  --ads-v2-h1-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h1-line-height: var(--ads-v2-line-height-8);
-  --ads-v2-h1-letter-spacing: var(--ads-v2-letter-spacing-2);
-
-  --ads-v2-h2-font-size: var(--ads-v2-font-size-8);
-  --ads-v2-h2-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h2-line-height: var(--ads-v2-line-height-7);
-  --ads-v2-h2-letter-spacing: var(--ads-v2-letter-spacing-2);
-
-  --ads-v2-h3-font-size: var(--ads-v2-font-size-7);
-  --ads-v2-h3-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h3-line-height: var(--ads-v2-line-height-6);
-  --ads-v2-h3-letter-spacing: var(--ads-v2-letter-spacing-2);
-
-  --ads-v2-h4-font-size: var(--ads-v2-font-size-6);
-  --ads-v2-h4-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h4-line-height: var(--ads-v2-line-height-3);
-  --ads-v2-h4-letter-spacing: var(--ads-v2-letter-spacing-0);
-
-  --ads-v2-h5-font-size: var(--ads-v2-font-size-4);
-  --ads-v2-h5-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h5-line-height: var(--ads-v2-line-height-3);
-  --ads-v2-h5-letter-spacing: var(--ads-v2-letter-spacing-0);
-
-  --ads-v2-h6-font-size: var(--ads-v2-font-size-2);
-  --ads-v2-h6-font-weight: var(--ads-v2-font-weight-bold);
-  --ads-v2-h6-line-height: var(--ads-v2-line-height-1);
-  --ads-v2-h6-letter-spacing: var(--ads-v2-letter-spacing-3);
-
-  --ads-v2-p-font-size: var(--ads-v2-font-size-4);
-  --ads-v2-p-font-weight: var(--ads-v2-font-weight-normal);
-  --ads-v2-p-line-height: var(--ads-v2-line-height-3);
-  --ads-v2-p-letter-spacing: var(--ads-v2-letter-spacing-0);
-`;
-
 const Variables = css`
   --font-family: var(--ads-v2-font-family);
   --color: var(--ads-v2-color-fg);
@@ -66,51 +29,69 @@ const Kinds = {
   code: css`
     --font-family: var(--ads-v2-font-family-code);
     --font-size: var(--ads-v2-font-size-4);
+    --font-weight: var(--ads-v2-font-weight-normal);
+    --line-height: var(--ads-v2-line-height-0);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "body-s": css`
     --font-size: var(--ads-v2-font-size-2);
+    --font-weight: var(--ads-v2-font-weight-normal);
+    --line-height: var(--ads-v2-line-height-1);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "body-m": css`
     --font-size: var(--ads-v2-font-size-4);
+    --font-weight: var(--ads-v2-font-weight-normal);
+    --line-height: var(--ads-v2-line-height-3);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "action-l": css`
     --font-size: var(--ads-v2-font-size-6);
+    --font-weight: var(--ads-v2-font-weight-extra-bold);
+    --line-height: var(--ads-v2-line-height-4);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "action-m": css`
     --font-size: var(--ads-v2-font-size-4);
+    --font-weight: var(--ads-v2-font-weight-extra-bold);
+    --line-height: var(--ads-v2-line-height-3);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "action-s": css`
     --font-size: var(--ads-v2-font-size-2);
+    --font-weight: var(--ads-v2-font-weight-bold);
+    --line-height: var(--ads-v2-line-height-1);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "heading-xl": css`
     --font-size: var(--ads-v2-font-size-14);
-    --font-weight: var(--ads-v2-h1-font-weight);
-    --line-height: var(--ads-v2-h1-line-height);
-    --letter-spacing: var(--ads-v2-h1-letter-spacing);
+    --font-weight: var(--ads-v2-font-weight-extra-bold);
+    --line-height: var(--ads-v2-line-height-3);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "heading-l": css`
     --font-size: var(--ads-v2-font-size-12);
-    --font-weight: var(--ads-v2-h2-font-weight);
-    --line-height: var(--ads-v2-h2-line-height);
-    --letter-spacing: var(--ads-v2-h2-letter-spacing);
+    --font-weight: var(--ads-v2-font-weight-extra-bold);
+    --line-height: var(--ads-v2-line-height-1);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "heading-m": css`
     --font-size: var(--ads-v2-font-size-10);
-    --font-weight: var(--ads-v2-h3-font-weight);
-    --line-height: var(--ads-v2-h3-line-height);
-    --letter-spacing: var(--ads-v2-h3-letter-spacing);
+    --font-weight: var(--ads-v2-font-weight-extra-bold);
+    --line-height: var(--ads-v2-line-height-2);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "heading-s": css`
     --font-size: var(--ads-v2-font-size-6);
-    --font-weight: var(--ads-v2-h4-font-weight);
-    --line-height: var(--ads-v2-h4-line-height);
-    --letter-spacing: var(--ads-v2-h4-letter-spacing);
+    --font-weight: var(--ads-v2-font-weight-bold);
+    --line-height: var(--ads-v2-line-height-4);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
   "heading-xs": css`
     --font-size: var(--ads-v2-font-size-4);
-    --font-weight: var(--ads-v2-h5-font-weight);
-    --line-height: var(--ads-v2-h5-line-height);
-    --letter-spacing: var(--ads-v2-h5-letter-spacing);
+    --font-weight: var(--ads-v2-font-weight-bold);
+    --line-height: var(--ads-v2-line-height-5);
+    --letter-spacing: var(--ads-v2-letter-spacing-0);
   `,
 };
 
@@ -118,7 +99,6 @@ export const StyledText = styled.span<{
   color?: string;
   kind?: Kind;
 }>`
-  ${TypographyScales}
   ${Variables}
 
   /* Variant style */
