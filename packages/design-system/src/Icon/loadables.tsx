116eff1115a5bd5b6e1046b6e2d2b7ee1269dcf8
Ivan Akulov <mail@iamakulov.com>
2023-03-09 21:55:43 +0100

    Update loadable wrappers to pass parent props to the fallback

--- packages/design-system/src/Icon/loadables.tsx
+++ packages/design-system/src/Icon/loadables.tsx
@@ -1,24 +1,43 @@
+// IMPORTANT: please keep this file in sync with packages/design-system-old/src/utils/icon-loadables.tsx.
+
 import React from "react";
 import loadable from "@loadable/component";
-import { RemixiconReactIconComponentType } from "remixicon-react";
+import {
+  RemixiconReactIconProps,
+  RemixiconReactIconComponentType,
+} from "remixicon-react";
 
-// Using `<svg />` as a loading fallback – to make sure sizes set by <IconContainer>
-// apply even while the full icon is still loading.
-const LOADING_FALLBACK = <svg />;
+function loadableForIconImpl(
+  importFn: () => Promise<any>,
+  options?: {
+    resolveComponent: (m: any) => React.ComponentType;
+  },
+) {
+  const IconLoader = loadable.lib(importFn, options as any);
+
+  return function AppsmithLoadable(props: any) {
+    return (
+      // Using `<svg />` as a loading fallback – to make sure sizes set by <IconWrapper>
+      // apply even while the full icon is still loading.
+      // And passing all props – if the parent component sets `width` or `height`,
+      // we want to make sure it’s applied to the loading fallback as well.
+      <IconLoader fallback={<svg {...props} />}>
+        {(Icon: React.ComponentType) => <Icon {...props} />}
+      </IconLoader>
+    );
+  };
+}
 
 export function loadableForSvg(
   importFn: () => Promise<typeof import("*.svg")>,
-) {
-  return loadable(importFn, {
-    resolveComponent: (m) => m.ReactComponent,
-    fallback: LOADING_FALLBACK,
+): React.ComponentType<React.SVGProps<SVGSVGElement>> {
+  return loadableForIconImpl(importFn, {
+    resolveComponent: (m: typeof import("*.svg")) => m.ReactComponent,
   });
 }
 
 export function loadableForRemixIcons(
   importFn: () => Promise<{ default: RemixiconReactIconComponentType }>,
-) {
-  return loadable(importFn, {
-    fallback: LOADING_FALLBACK,
-  });
+): React.ComponentType<RemixiconReactIconProps> {
+  return loadableForIconImpl(importFn);
 }
