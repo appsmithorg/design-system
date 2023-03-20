9418d88fb0a519b13a7605832b429992e457ce91
Ivan Akulov <mail@iamakulov.com>
2023-03-20 14:32:14 +0100

    Replace @loadable/component with <Suspense>

--- packages/design-system/src/Icon/loadables.tsx
+++ packages/design-system/src/Icon/loadables.tsx
@@ -1,19 +1,15 @@
 // IMPORTANT: please keep this file in sync with packages/design-system-old/src/utils/icon-loadables.tsx.
 
-import React from "react";
-import loadable from "@loadable/component";
+import React, { Suspense } from "react";
 import {
   RemixiconReactIconProps,
   RemixiconReactIconComponentType,
 } from "remixicon-react";
 
 function importIconImpl(
-  importFn: () => Promise<any>,
-  options?: {
-    resolveComponent: (m: any) => React.ComponentType;
-  },
+  importFn: () => Promise<{ default: React.ComponentType }>,
 ) {
-  const IconLoader = loadable.lib(importFn, options as any);
+  const Icon = React.lazy(importFn);
 
   return function AppsmithLoadable(props: any) {
     return (
@@ -21,9 +17,9 @@ function importIconImpl(
       // apply even while the full icon is still loading.
       // And passing all props – if the parent component sets `width` or `height`,
       // we want to make sure it’s applied to the loading fallback as well.
-      <IconLoader fallback={<svg {...props} />}>
-        {(Icon: React.ComponentType) => <Icon {...props} />}
-      </IconLoader>
+      <Suspense fallback={<svg {...props} />}>
+        <Icon {...props} />
+      </Suspense>
     );
   };
 }
@@ -31,9 +27,9 @@ function importIconImpl(
 export function importSvg(
   importFn: () => Promise<typeof import("*.svg")>,
 ): React.ComponentType<React.SVGProps<SVGSVGElement>> {
-  return importIconImpl(importFn, {
-    resolveComponent: (m: typeof import("*.svg")) => m.ReactComponent,
-  });
+  return importIconImpl(() =>
+    importFn().then((m) => ({ default: m.ReactComponent })),
+  );
 }
 
 export function importRemixIcon(
