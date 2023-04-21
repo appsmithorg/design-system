30e96edb943615438a37fd7d2fa17d8f3599fa3b
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2023-04-21 11:26:01 +0530

    enabled ads v2 variables inside old ds

--- packages/design-system-old/.storybook/preview.js
+++ packages/design-system-old/.storybook/preview.js
@@ -3,6 +3,8 @@ import "normalize.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "./styles.css";
+import "../src/themes/default/index.css";
+import "../../design-system/src/__theme__/default/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
