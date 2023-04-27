2ad51c81e1bf1ba8f4615c71262cf743f2c40dde
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-04-27 17:28:12 +0530

    extract types of input

--- packages/design-system/src/Input/Input.types.tsx
+++ packages/design-system/src/Input/Input.types.tsx
@@ -4,6 +4,15 @@ import { Sizes } from "__config__/types";
 
 export type InputSizes = Extract<Sizes, "sm" | "md">;
 
+export type InputType =
+  | "text"
+  | "password"
+  | "email"
+  | "number"
+  | "tel"
+  | "url"
+  | undefined;
+
 // Input props
 export interface InputProps extends TextFieldProps {
   /** TODO: renderAs needs to changed to as */
@@ -26,7 +35,7 @@ export interface InputProps extends TextFieldProps {
   /** size */
   size?: InputSizes;
   /** type */
-  type?: "text" | "password" | "email" | "number" | "tel" | "url";
+  type?: InputType;
   /** width */
   UNSAFE_width?: string;
   /** height */
