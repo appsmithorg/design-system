f1c9d122873778de0adce6b522fa9a09e0e1c4e5
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-05-01 13:56:30 +0530

    Add story to show how both menu and tooltip can be used together.

--- packages/design-system/src/Menu/Menu.stories.tsx
+++ packages/design-system/src/Menu/Menu.stories.tsx
@@ -15,6 +15,7 @@ import {
 } from "./Menu";
 import { Button } from "../Button";
 import { Text } from "../Text";
+import { Tooltip } from "../Tooltip";
 
 export default {
   title: "Design System/Menu",
@@ -26,13 +27,7 @@ const Template: ComponentStory<typeof Menu> = () => {
   return (
     <Menu>
       <MenuTrigger>
-        <Button
-          isIconButton
-          kind="tertiary"
-          size="sm"
-          startIcon="comment-context-menu"
-        />
-        {/*<Button>Menu</Button>*/}
+        <Button>Menu</Button>
       </MenuTrigger>
       <MenuContent loop width="200px">
         <MenuItem startIcon="settings-line">
@@ -463,3 +458,25 @@ MenuSubmenuStory.argTypes = {
     },
   },
 };
+
+export function MenuWithTooltipOnTrigger() {
+  return (
+    <Menu>
+      <Tooltip
+        content={
+          "this tooltip appears when you hover over the menu trigger. " +
+          "clicking the menu trigger will open the menu only if the tooltip is " +
+          "wrapped around the MenuTrigger and not the other way around."
+        }
+      >
+        <MenuTrigger>
+          <Button>Click me</Button>
+        </MenuTrigger>
+      </Tooltip>
+      <MenuContent width="150px">
+        <MenuItem>Item</MenuItem>
+        <MenuItem>Item</MenuItem>
+      </MenuContent>
+    </Menu>
+  );
+}
