78ff71ffd983b53aa5aa8906e0bcf1332fba8283
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-07-11 10:59:24 +0530

    Add new story types

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -43,7 +43,7 @@ async function webpackConfig(config, options) {
           svgoConfig: {
             plugins: [
               {
-                name: 'preset-default',
+                name: "preset-default",
                 params: {
                   overrides: {
                     removeViewBox: false,
@@ -79,7 +79,11 @@ module.exports = {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/*.mdx",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
@@ -100,5 +104,6 @@ module.exports = {
   },
   docs: {
     autodocs: true,
+    defaultName: "Documentation",
   },
 };
