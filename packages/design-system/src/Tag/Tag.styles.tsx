c2249d1b0d4c9591f6f72187c22143d256711cfd
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-07-26 19:44:20 +0530

    Remove unnecessary variable

--- packages/design-system/src/Tag/Tag.styles.tsx
+++ packages/design-system/src/Tag/Tag.styles.tsx
@@ -6,7 +6,6 @@ const Variables = css`
   --tag-color-background: var(--ads-v2-color-bg);
   --tag-color-border: var(--ads-v2-color-border);
   --tag-color-fg: var(--ads-v2-color-fg);
-  --tag-close-icon-color-background: inherit;
 `;
 
 const SizeMap = {
@@ -25,37 +24,31 @@ const KindMap = {
     --tag-color-background: var(--ads-v2-color-bg-success);
     --tag-color-border: var(--ads-v2-color-border-success);
     --tag-color-fg: var(--ads-v2-color-fg-success);
-    --tag-close-icon-color-background: inherit;
   `,
   warning: css`
     --tag-color-background: var(--ads-v2-color-bg-warning);
     --tag-color-border: var(--ads-v2-color-border-warning);
     --tag-color-fg: var(--ads-v2-color-fg-warning);
-    --tag-close-icon-color-background: inherit;
   `,
   info: css`
     --tag-color-background: var(--ads-v2-color-bg-information);
     --tag-color-border: var(--ads-v2-color-border-info);
     --tag-color-fg: var(--ads-v2-color-fg-information);
-    --tag-close-icon-color-background: inherit;
   `,
   error: css`
     --tag-color-background: var(--ads-v2-color-bg-error);
     --tag-color-border: var(--ads-v2-color-border-error);
     --tag-color-fg: var(--ads-v2-color-fg-error);
-    --tag-close-icon-color-background: inherit;
   `,
   special: css`
     --tag-color-background: var(--ads-v2-color-bg-special);
     --tag-color-border: var(--ads-v2-color-border-special);
     --tag-color-fg: var(--ads-v2-color-fg-special);
-    --tag-close-icon-color-background: inherit;
   `,
   premium: css`
     --tag-color-background: var(--ads-v2-color-bg-premium);
     --tag-color-border: var(--ads-v2-color-border-premium);
     --tag-color-fg: var(--ads-v2-color-fg-premium);
-    --tag-close-icon-color-background: inherit;
   `,
 };
 
@@ -85,7 +78,7 @@ export const StyledTag = styled.span<{
 
 export const StyledButton = styled(Button)`
   --button-color-fg: var(--tag-color-fg);
-  --button-color-bg: var(--tag-close-icon-color-backgrond);
+  --button-color-bg: inherit;
 
   margin-left: var(--ads-v2-spaces-1);
   position: relative;
