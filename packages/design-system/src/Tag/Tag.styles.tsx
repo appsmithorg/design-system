b7d56b2fb6c23934c1ec710d4fcd68a953bdbdc2
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2023-08-04 14:50:48 +0530

    Tag updates for category token

--- packages/design-system/src/Tag/Tag.styles.tsx
+++ packages/design-system/src/Tag/Tag.styles.tsx
@@ -21,34 +21,34 @@ const SizeMap = {
 
 const KindMap = {
   success: css`
-    --tag-color-background: var(--ads-v2-color-bg-success);
-    --tag-color-border: var(--ads-v2-color-border-success);
-    --tag-color-fg: var(--ads-v2-color-fg-success);
+    --tag-color-background: var(--ads-v2-colors-content-surface-success-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-success-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-success-fg);
   `,
   warning: css`
-    --tag-color-background: var(--ads-v2-color-bg-warning);
-    --tag-color-border: var(--ads-v2-color-border-warning);
-    --tag-color-fg: var(--ads-v2-color-fg-warning);
+    --tag-color-background: var(--ads-v2-colors-content-surface-warning-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-warning-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-warning-fg);
   `,
   info: css`
-    --tag-color-background: var(--ads-v2-color-bg-information);
-    --tag-color-border: var(--ads-v2-color-border-info);
-    --tag-color-fg: var(--ads-v2-color-fg-information);
+    --tag-color-background: var(--ads-v2-colors-content-surface-info-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-info-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-info-fg);
   `,
   error: css`
-    --tag-color-background: var(--ads-v2-color-bg-error);
-    --tag-color-border: var(--ads-v2-color-border-error);
-    --tag-color-fg: var(--ads-v2-color-fg-error);
+    --tag-color-background: var(--ads-v2-colors-content-surface-error-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-error-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-error-fg);
   `,
   special: css`
-    --tag-color-background: var(--ads-v2-color-bg-special);
-    --tag-color-border: var(--ads-v2-color-border-special);
-    --tag-color-fg: var(--ads-v2-color-fg-special);
+    --tag-color-background: var(--ads-v2-colors-content-surface-special-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-special-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-special-fg);
   `,
   premium: css`
-    --tag-color-background: var(--ads-v2-color-bg-premium);
-    --tag-color-border: var(--ads-v2-color-border-premium);
-    --tag-color-fg: var(--ads-v2-color-fg-premium);
+    --tag-color-background: var(--ads-v2-colors-content-surface-premium-bg);
+    --tag-color-border: var(--ads-v2-colors-content-surface-premium-border);
+    --tag-color-fg: var(--ads-v2-colors-content-label-premium-fg);
   `,
 };
 
