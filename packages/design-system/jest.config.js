d62671c5a162953ed201bbc229f9bb2422f15240
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-08-29 08:03:34 +0530

    set up jest

---
+++ packages/design-system/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ["<rootDir>/src"], // Set this to the directory containing your source code
+  // setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Optional: Additional setup
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest", // Use ts-jest for transforming TypeScript files
+    // '\\.(css|scss|sass)$': 'identity-obj-proxy', // Use identity-obj-proxy for handling CSS modules
+    // '\\.(svg)$': '<rootDir>/fileTransformer.js', // Create this file for SVG handling (see below)
+  },
+  // moduleNameMapper: {
+  // "\\.(css|scss|sass)$": "identity-obj-proxy", // Match CSS imports to the proxy
+  // },
+};
