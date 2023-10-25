0753e7a754a8865d7d1660fd09f46682a8ecb1d3
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2023-10-25 13:55:02 +0530

    fix: table header height fix

--- packages/design-system/src/Table/Table.styles.tsx
+++ packages/design-system/src/Table/Table.styles.tsx
@@ -35,6 +35,7 @@ export const StyledHeaderCell = styled.th.attrs(({ className }) => ({
     font-size: var(--ads-v2-font-size-4);
     font-style: normal;
     font-weight: var(--ads-v2-font-weight-normal);
+    line-height: 13px;
     color: var(--ads-v2-colors-content-label-default-fg);
     font-family: var(--ads-v2-font-family);
     border: none;
@@ -65,6 +66,7 @@ export const StyledCell = styled.td.attrs(({ className }) => ({
     color: var(--ads-v2-colors-content-label-default-fg);
     font-family: var(--ads-v2-font-family);
     border: none;
+    line-height: normal;
   }
 
   &&.rc-table-cell.rc-table-cell-row-hover {
