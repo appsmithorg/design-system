1048734564f0a2ccda0c87c86a1a7b70e9291fad
vaibhavchobisa <vaibhav.chobisa@gmail.com>
2023-12-18 19:20:58 +0530

    resolves #26847

--- packages/design-system/src/Documentation/components/CodeBlock.tsx
+++ packages/design-system/src/Documentation/components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Tooltip } from "Tooltip";
+import { Icon } from "Icon";
 import { toast } from "Toast";
 
 type CodeBlockProps = {
@@ -15,16 +15,14 @@ function CodeBlock({ code }: CodeBlockProps) {
         toast.show("Copied to clipboard", { kind: "success" });
       }}
     >
-      <Tooltip content="Click here to copy" mouseEnterDelay={0.7}>
-        <Code>{code}</Code>
-      </Tooltip>
+      <Code>{code}&nbsp;</Code>
+      <Icon name="copy-control" size="md" />
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
   cursor: pointer;
   padding: 0 4px;
   border-radius: var(--ads-v2-border-radius);
