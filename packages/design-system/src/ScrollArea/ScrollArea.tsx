a1fc69a00ecf6fe6636543a583da3516f316a0f5
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-04-23 01:00:14 +0530

    fix: options extraction

--- packages/design-system/src/ScrollArea/ScrollArea.tsx
+++ packages/design-system/src/ScrollArea/ScrollArea.tsx
@@ -7,19 +7,28 @@ import "./styles.css";
 
 import { ScrollAreaProps } from "./ScrollArea.types";
 
-function ScrollArea({ children, size = "md", ...rest }: ScrollAreaProps) {
+function ScrollArea({
+  children,
+  options,
+  size = "md",
+  ...rest
+}: ScrollAreaProps) {
+  const defaultOptions: Pick<ScrollAreaProps, "options"> = {
+    options: {
+      scrollbars: {
+        theme: "ads-v2-scroll-theme",
+        autoHide: "scroll",
+      },
+      ...options,
+    },
+  };
   return (
     <OverlayScrollbarsComponent
       className={clsx({
         "scroll-sm": size === "sm",
       })}
       defer
-      options={{
-        scrollbars: {
-          theme: "ads-v2-scroll-theme",
-          autoHide: "scroll",
-        },
-      }}
+      options={defaultOptions.options}
       {...rest}
     >
       {children}
